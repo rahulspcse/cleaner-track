94c7d9abf76688809f243f8d5348c672329a5b34
rahuliww <105700024+rahuliww@users.noreply.github.com>
2022-05-18 12:56:48 +0600

    updated

--- src/Components/StartEndJob/StartEndJob.jsx
+++ src/Components/StartEndJob/StartEndJob.jsx
@@ -2,17 +2,16 @@ import { useParams } from "react-router-dom";
 import jobsData from "../../jobsData";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import InputLabel from '@mui/material/InputLabel';
-import TextField from '@mui/material/TextField';
+import InputLabel from "@mui/material/InputLabel";
+import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import { createRef, useContext, useEffect, useState } from "react";
 import moment from "moment";
 import useGeoLocation from "../useGeoLocation";
 import { JobsContext } from "../../App";
-import CameraAltOutlinedIcon from '@mui/icons-material/CameraAltOutlined';
+import CameraAltOutlinedIcon from "@mui/icons-material/CameraAltOutlined";
 
 export default function StartEndJob() {
-
   const [data, setData] = useContext(JobsContext);
   const [preview, setPreview] = useState();
   const [file, setFile] = useState();
@@ -29,9 +28,9 @@ export default function StartEndJob() {
     setFile(e.target.files);
     setPreview(URL.createObjectURL(e.target.files[0]));
   };
-  
+
   const location = useGeoLocation();
-  const [value, setValue] = useState({ 
+  const [value, setValue] = useState({
     lat: 22.33895932186688,
     lng: 91.84356541651367,
     distance: 500,
@@ -40,7 +39,6 @@ export default function StartEndJob() {
 
   const [result, setResult] = useState();
 
- 
   useEffect(() => {
     if (location.loaded && location.coordinates) {
       const center = new window.google.maps.LatLng(value.lat, value.lng);
@@ -71,9 +69,7 @@ export default function StartEndJob() {
     }
   }, [location]);
 
-
   const handleCheck = () => {
-
     if (location.loaded && location.coordinates) {
       const center = new window.google.maps.LatLng(value.lat, value.lng);
 
@@ -101,52 +97,51 @@ export default function StartEndJob() {
         setResult(false);
       }
     }
-  }
- 
-  const [startJobDetails, setStartJobDetails]= useState(
-    {
-      startTime: '',
-      location: '',
-    }
-  );
+  };
 
-  const startJobSubmit=()=>{
+  const [startJobDetails, setStartJobDetails] = useState({
+    startTime: "",
+    location: "",
+  });
+
+  const startJobSubmit = () => {
     const startCurrentLocation = location;
     const newObj = {};
-    newObj.location= startCurrentLocation;
-    newObj.startTime= moment().format('DD-MM-YYYY h:mm:ss a');
-    job.startDetails= newObj;
-    job.status= true;
+    newObj.location = startCurrentLocation;
+    newObj.startTime = moment().format("DD-MM-YYYY h:mm:ss a");
+    job.startDetails = newObj;
+    job.status = true;
     const temp_data = data.filter((item) => item.id != id);
     setData([...temp_data, job]);
     setStartJobDetails(newObj);
-  }
+  };
 
-  const [endJobDetails, setEndJobDetails]= useState(
-    {
-      endTime: '',
-      location: '',
-    }
-  );
+  const [endJobDetails, setEndJobDetails] = useState({
+    endTime: "",
+    location: "",
+  });
 
-  const [totalTime, setTotalTime]= useState();
+  const [totalTime, setTotalTime] = useState();
 
-  const endJobSubmit=()=>{
+  const endJobSubmit = () => {
     const endCurrentLocation = location;
     const newObj = {};
-    newObj.location= endCurrentLocation;
-    newObj.endTime= moment().format('DD-MM-YYYY h:mm:ss a');
-    const tempObj= moment.duration(moment(newObj.endTime, 'DD-MM-YYYY h:mm:ss a').diff(moment(job.startDetails.startTime, 'DD-MM-YYYY h:mm:ss a').format()))._data;
-    const temp_total= `${tempObj.hours} hours ${tempObj.minutes} minutes ${tempObj.seconds} seconds`;
+    newObj.location = endCurrentLocation;
+    newObj.endTime = moment().format("DD-MM-YYYY h:mm:ss a");
+    const tempObj = moment.duration(
+      moment(newObj.endTime, "DD-MM-YYYY h:mm:ss a").diff(
+        moment(job.startDetails.startTime, "DD-MM-YYYY h:mm:ss a").format()
+      )
+    )._data;
+    const temp_total = `${tempObj.hours} hours ${tempObj.minutes} minutes ${tempObj.seconds} seconds`;
     job.endDetails = newObj;
-    job.totalTime= temp_total;
-    job.status= 'completed';
+    job.totalTime = temp_total;
+    job.status = "completed";
     const temp_data = data.filter((item) => item.id != id);
     setData([...temp_data, job]);
     setTotalTime(temp_total);
     setEndJobDetails(newObj);
-  }
-
+  };
 
   return (
     <Box
@@ -154,8 +149,8 @@ export default function StartEndJob() {
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
-        bgcolor: 'black',
-        height: '100vh'
+        bgcolor: "black",
+        height: "100vh",
       }}
     >
       <Typography
@@ -164,7 +159,7 @@ export default function StartEndJob() {
           fontWeight: "bold",
           fontSize: "35px",
           my: 4,
-          color: '#ffffff'
+          color: "#ffffff",
         }}
       >
         {job.name}
@@ -181,80 +176,122 @@ export default function StartEndJob() {
         <Button sx={{my: 2}} onClick={handleCheck} variant="contained">Submit</Button>
         <h3>Output: {difference}</h3>
       </Box> */}
-     { !file &&
-      <CameraAltOutlinedIcon sx={{color: '#ffffff', fontSize: '40px', '&:hover': {cursor: "pointer"}}} onClick={(e) => click(e)}  />
-     }
-      <input style={{ display: "none" }} ref={fileRef}  type="file" onChange={(e) => handlePreview(e)} />
-      {
-        file &&  <img style={{ objectFit: 'contain', marginBottom: '30px', height: '300px', width: '350px'}} src={preview} />
-      }
-      {
-        file && 
+      {!file && (
+        <CameraAltOutlinedIcon
+          sx={{
+            color: "#ffffff",
+            fontSize: "40px",
+            "&:hover": { cursor: "pointer" },
+          }}
+          onClick={(e) => click(e)}
+        />
+      )}
+      <input
+        style={{ display: "none" }}
+        ref={fileRef}
+        type="file"
+        onChange={(e) => handlePreview(e)}
+      />
+      {file && (
+        <img
+          style={{
+            objectFit: "contain",
+            marginBottom: "30px",
+            height: "300px",
+            width: "350px",
+          }}
+          src={preview}
+        />
+      )}
+      {file && (
         <Button
-        sx={{ width: 150, my: 2, bgcolor: '#4383BB',  '&:hover': {bgcolor: '#4383BB'} }}
-        variant="contained"
-        color="success"
-        onClick={(e) => click(e)}
+          sx={{
+            width: 150,
+            my: 2,
+            bgcolor: "#4383BB",
+            "&:hover": { bgcolor: "#4383BB" },
+          }}
+          variant="contained"
+          color="success"
+          onClick={(e) => click(e)}
         >
-        Retake Photo
+          Retake Photo
         </Button>
-      }
-     
+      )}
 
-     <Typography
-        sx={{
-          textAlign: "center",
-          fontSize: "14px",
-          my: 2,
-          color: '#ffffff'
-        }}
-      >
-        Take a snap or upload an image
-        then start the job
-      </Typography>
-
-      {
-        !result && 
+      {job.status ? (
         <Typography
-        sx={{
-          textAlign: "center",
-          fontWeight: "bold",
-          fontSize: {xs: "14px", sm: "20px"},
-          color: 'red',
-          my: 4,
-        }}
-      >
-        You need to be in the site before starting a job
-      </Typography>
+          sx={{
+            textAlign: "center",
+            fontSize: "14px",
+            my: 2,
+            color: "#ffffff",
+          }}
+        >
+          Take a snap or upload an image then End the job
+        </Typography>
+      ) : (
+        <Typography
+          sx={{
+            textAlign: "center",
+            fontSize: "14px",
+            my: 2,
+            color: "#ffffff",
+          }}
+        >
+          Take a snap or upload an image then Start the job
+        </Typography>
+      )}
 
-      }
+      {!result && (
+        <Typography
+          sx={{
+            textAlign: "center",
+            fontWeight: "bold",
+            fontSize: { xs: "14px", sm: "20px" },
+            color: "red",
+            my: 4,
+          }}
+        >
+          You need to be in the site before starting a job
+        </Typography>
+      )}
 
-      { 
-      result ? 
-      job.status == false ? 
-      <Button
-      sx={{ width: 250, mb: 2, bgcolor: '#3D3D96', '&:hover': {bgcolor: '#3D3D96'} }}
-      variant="contained"
-      color="success"
-      onClick={startJobSubmit}
-      >
-      Start Job
-      </Button>
-      :
-      <Button
-      sx={{ width: 250, mb: 2, bgcolor: '#C74F4F',  '&:hover': {bgcolor: '#C74F4F'} }}
-      variant="contained"
-      color="success"
-      onClick={endJobSubmit}
-      >
-      End Job
-      </Button>
-      :
-      <></>
-      }
-           
-      
-{/* 
+      {result ? (
+        job.status == false ? (
+          <Button
+            sx={{
+              width: 250,
+              mb: 2,
+              bgcolor: "#3D3D96",
+              "&:hover": { bgcolor: "#3D3D96" },
+            }}
+            variant="contained"
+            color="success"
+            onClick={startJobSubmit}
+          >
+            Start Job
+          </Button>
+        ) : (
+          <Button
+            sx={{
+              width: 250,
+              mb: 2,
+              bgcolor: "#C74F4F",
+              "&:hover": { bgcolor: "#C74F4F" },
+            }}
+            variant="contained"
+            color="success"
+            onClick={endJobSubmit}
+          >
+            End Job
+          </Button>
+        )
+      ) : (
+        <></>
+      )}
+
+      {/* 
         {
         startJobDetails.location ?
         <Box sx={{m:5}}>
@@ -278,7 +315,6 @@ export default function StartEndJob() {
         :
         <></>
         } */}
-      
     </Box>
   );
 }
